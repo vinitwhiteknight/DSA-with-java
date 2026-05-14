812f526113f00a3bebfdd647e8206c981a24c83f
Vinit Burkul <vinitburkul818310@gmail.com>
2026-05-14 21:03:22 +0530

    Create ContainerWithMostWater.java

---
+++ Day-2/ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right= height.length-1;
+        int maxarea=0;
+
+        while(left<=right){
+            maxarea=Math.max(maxarea, ((right-left)*Math.min(height[left],height[right])));
+
+            if (height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+
+            }
+        }
+        return maxarea;
+    }
+}
