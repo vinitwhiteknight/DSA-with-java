b4e2c567a223e4269d07e85c056cb1c2116feae3
Vinit Burkul <vinitburkul818310@gmail.com>
2026-06-10 16:47:57 +0530

    Create NumberOfIslands.java

---
+++ Day-28/NumberOfIslands.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        int numIslands = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    queue.offer(new int[]{i, j});
+                    
+                    while (!queue.isEmpty()) {
+                        int[] curr = queue.poll();
+                        int x = curr[0];
+                        int y = curr[1];
+                        
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') {
+                            continue;
+                        }
+                        
+                        grid[x][y] = '0'; // mark as visited
+                        
+                        for (int[] dir : directions) {
+                            int nx = x + dir[0];
+                            int ny = y + dir[1];
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
+                                queue.offer(new int[]{nx, ny});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return numIslands;
+    }
+}
