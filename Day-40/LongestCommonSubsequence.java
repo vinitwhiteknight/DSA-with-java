17c5a724b5e60ab2dae576fd0c62561d58d6ba13
Vinit Burkul <vinitburkul818310@gmail.com>
2026-06-22 12:16:50 +0530

    LongestCommonSubsequence.java

--- Day-40/LongestCommonSubsequence.java
+++ Day-40/LongestCommonSubsequence.java
@@ -5,3 +5,12 @@ class Solution {
         for(int i = 0; i <= m; i++) dp[i][0] = 0;
         for(int j = 0; j <= n; j++) dp[0][j] = 0;
 
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+}
