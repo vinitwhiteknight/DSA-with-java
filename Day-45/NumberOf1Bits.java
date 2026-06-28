aac0a8d167c8765903e5672b8aa7711e76b1593f
Vinit Burkul <vinitburkul818310@gmail.com>
2026-06-28 17:53:19 +0530

    Create NumberOf1Bits.java

---
+++ Day-45/NumberOf1Bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hammingWeight(int n) {
+        
+        int cnt=0;
+
+        while(n!=0)
+        {
+           cnt++;
+           n=n&(n-1); 
+        }
+
+        return cnt;
+    }
+}
