a6215ae126588c3a67ef95556eb0b17f1959a434
Vinit Burkul <vinitburkul818310@gmail.com>
2026-07-02 18:37:12 +0530

    Create BinaryNumberWithAlternatingBits.java

---
+++ Day-49/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int currBit = n % 2;
+        n /= 2;
+
+        while (n > 0) {
+            if (currBit == n % 2) {
+                return false;
+            }
+
+            currBit = n % 2;
+            n /= 2;
+        }
+
+        return true;
+    }
+}
